c2b22410f6b21778e5a13ccad7af19955709f652
Juan RP <xtraeme@gmail.com>
2019-12-29 11:58:49 +0100

    src/rb.c: merge NetBSD HEAD revs 1.14 and 1.15.
    http://cvsweb.netbsd.org/bsdweb.cgi/src/common/lib/libc/gen/rb.c?only_with_tag=MAIN

--- src/rb.c
+++ src/rb.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: rb.c,v 1.14 2019/03/08 09:14:54 roy Exp $	*/
+/*	$NetBSD: rb.c,v 1.15 2019/05/09 10:56:24 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2001 The NetBSD Foundation, Inc.
@@ -29,14 +29,47 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#if HAVE_NBTOOL_CONFIG_H
+#include "nbtool_config.h"
+#endif
+
+#if !defined(_KERNEL) && !defined(_STANDALONE)
 #include <sys/types.h>
 #include <stddef.h>
 #include <assert.h>
 #include <stdbool.h>
 #ifdef RBDEBUG
 #define	KASSERT(s)	assert(s)
+#define	__rbt_unused
 #else
 #define KASSERT(s)	do { } while (/*CONSTCOND*/ 0)
+#define	__rbt_unused	__unused
+#endif
+__RCSID("$NetBSD: rb.c,v 1.15 2019/05/09 10:56:24 skrll Exp $");
+#else
+#include <lib/libkern/libkern.h>
+__KERNEL_RCSID(0, "$NetBSD: rb.c,v 1.15 2019/05/09 10:56:24 skrll Exp $");
+#ifndef DIAGNOSTIC
+#define	__rbt_unused	__unused
+#else
+#define	__rbt_unused
+#endif
+#endif
+
+#ifdef _LIBC
+__weak_alias(rb_tree_init, _rb_tree_init)
+__weak_alias(rb_tree_find_node, _rb_tree_find_node)
+__weak_alias(rb_tree_find_node_geq, _rb_tree_find_node_geq)
+__weak_alias(rb_tree_find_node_leq, _rb_tree_find_node_leq)
+__weak_alias(rb_tree_insert_node, _rb_tree_insert_node)
+__weak_alias(rb_tree_remove_node, _rb_tree_remove_node)
+__weak_alias(rb_tree_iterate, _rb_tree_iterate)
+#ifdef RBDEBUG
+__weak_alias(rb_tree_check, _rb_tree_check)
+__weak_alias(rb_tree_depths, _rb_tree_depths)
+#endif
+
+#include "namespace.h"
 #endif
 
 #ifdef RBTEST
@@ -291,10 +324,9 @@ rb_tree_insert_node(struct rb_tree *rbt, void *object)
  * removal since rotation almost always involves the exchanging of colors
  * as a separate step.
  */
-/*ARGSUSED*/
 static void
-rb_tree_reparent_nodes(struct rb_tree *rbt, struct rb_node *old_father,
-	const unsigned int which)
+rb_tree_reparent_nodes(__rbt_unused struct rb_tree *rbt,
+	struct rb_node *old_father, const unsigned int which)
 {
 	const unsigned int other = which ^ RB_DIR_OTHER;
 	struct rb_node * const grandpa = RB_FATHER(old_father);
